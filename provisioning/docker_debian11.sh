a4e81ce4ef789533b36195c0fe644dd34d7c9ac4
Nick Schlobohm <schlobohmnick@gmail.com>
2022-04-13 10:13:16 +1000

    add: docker_debian11.sh

---
+++ provisioning/docker_debian11.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# [[ Following guide from https://docs.docker.com/engine/install/debian/ as at 13 April 2022 ]]
+
+# Uninstall old versions
+# -- Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them:
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+# Install using the repository
+# -- Set up the repository
+# ---- Update apt package index
+sudo apt-get update
+
+# ---- Install packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+# ---- Add Docker GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# ---- Set up the stable repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# -- Install Docker Engine
+# ---- Update apt package index
+sudo apt-get update
+
+# ---- Install latest version of Docker Engine and containerd
+sudo apt-get install docker-ce docker-ce-cli containerd.io
