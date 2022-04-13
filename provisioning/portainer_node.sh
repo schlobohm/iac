de104dd98f1d8f87b99afdf056f18fdb51052680
Nick Schlobohm <schlobohmnick@gmail.com>
2022-04-13 21:07:41 +1000

    add: portainer_node.sh

---
+++ provisioning/portainer_node.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:latest
+
+# optional - hardcode version in image tag
